1bd9bffffab76a0e9272a9143c2deee11c06d645
Eric Hernandez <erichernandez@Erics-MBP.home>
2022-12-04 15:09:41 -0600

    readMessage working reading from dynamoDB

--- amplify/backend/function/readMessage/src/index.js
+++ amplify/backend/function/readMessage/src/index.js
@@ -5,10 +5,18 @@ Amplify Params - DO NOT EDIT */
 const moment = require("moment");
 const ddbGeo = require("dynamodb-geo");
 const AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient({ region: 'us-east-1'});
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 exports.handler = async (event) => {
+    await readMessage().then(data => {
+        data.Items.forEach(function(item){
+            console.log(item.geohash)
+        })
+    }).catch((err) => {
+        console.error(err);
+    })
     console.log(`EVENT: ${JSON.stringify(event)}`);
     return {
         statusCode: 200,
@@ -17,6 +25,14 @@ exports.handler = async (event) => {
     //      "Access-Control-Allow-Origin": "*",
     //      "Access-Control-Allow-Headers": "*"
     //  }, 
-    body: JSON.stringify(moment().format()),
+    // body: JSON.stringify(moment().format()),
     };
 };
+
+function readMessage(){
+    const params = {
+        TableName: 'askJames-wheresStarbucks',
+        Limit: 10
+    }
+    return ddb.scan(params).promise()
+}
